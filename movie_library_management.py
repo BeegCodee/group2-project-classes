a3e350002df07654f2c4f1b7714df9f24a59e007
Dylan Dang <dyland9576@gmail.com>
2025-04-10 11:40:43 -0600

    adds all required files for project

---
+++ movie_library_management.py
@@ -0,0 +1,9 @@
+'''
+Project Name: movie_library_management.py
+Description: Python application that manages movie intentory and rental processes.
+    Allows the user to add, update, remove, serach for, rent, and return movies.
+    The application also allows the user to list movies by genre, list the top rented movies,
+    check for movies' availability by genre, and display a summary of the movies' library.
+Authors: Dylan Dang, Taylor Blair-Kril
+Date: April 10, 2025
+'''
